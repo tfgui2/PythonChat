dc24cfd9b94054fc45cbbf2f4a09e1771443ad63
tfgui <tfgui2@gmail.com>
2019-09-02 00:44:02 +0900

    socketserver

--- PythonChatClient/PythonChatClient.py
+++ PythonChatClient/PythonChatClient.py
@@ -1,2 +1,17 @@
+import socket
+import sys
 
-print("client~")
+HOST, PORT = "localhost", 9999
+data = " ".join(sys.argv[1:])
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
+while True:
+    data = input(">")
+    sock.sendall(bytes(data + "\n", "utf-8"))
+    received = str(sock.recv(1024), "utf-8")
+
+    print("Sent:     {}".format(data))
+    print("Received: {}".format(received))
