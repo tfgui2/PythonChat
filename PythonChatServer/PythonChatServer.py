8f8fc0beedc1f3aa2038ae7e79812dddd4a50fcc
tfgui <tfgui2@gmail.com>
2020-08-20 02:26:24 +0900

    a

--- PythonChatServer/PythonChatServer.py
+++ PythonChatServer/PythonChatServer.py
@@ -1,5 +1,7 @@
 import socketserver
 
+clients = {}
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
     The request handler class for our server.
@@ -10,18 +12,24 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     """
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        print("client connect {}".format(self.client_address))
+        clients += self.request
+        while True:
+            # self.request is the TCP socket connected to the client
+            self.data = self.request.recv(1024).strip()
+            print("{} wrote:".format(self.client_address))
+            print(self.data)
+
+            # just send back the same data, but upper-cased
+            for c in clients:
+                c.sendall(self.data.upper())
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
 
     # Create the server, binding to localhost on port 9999
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+    with socketserver.ThreadingTCPServer((HOST, PORT), MyTCPHandler) as server:
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
+        print("Server Start...")
         server.serve_forever()
